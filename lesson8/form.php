1343a7b4f1eece1686e7661e0c45b5109d77fcd7
Agent-003 <abbasova003@gmail.com>
2019-11-05 17:38:47 +0200

    remove index.php , update form.php

--- lesson8/form.php
+++ lesson8/form.php
@@ -9,25 +9,17 @@
     <title>Signin Template · Bootstrap</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/sign-in/">
-
 </head>
 <body class="text-center">
 <div class="container">
     <div class="row ">
-
-        <form class="form-signin">
+        <form class="form-signin" method="post" action="script.php">
             <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
             <label for="inputEmail" class="sr-only">Email address</label>
-            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+            <input type="text" name="name" id="inputEmail" class="form-control" placeholder="Email address" autofocus>
             <label for="inputPassword" class="sr-only">Password</label>
-            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-            <div class="checkbox mb-3">
-                <label>
-                    <input type="checkbox" value="remember-me"> Remember me
-                </label>
-            </div>
+            <input type="password" name="pass" id="inputPassword" class="form-control" placeholder="Password">
+
             <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
         </form>
     </div>
